b29b5e9b89a3a50541f2a650d6559f3b2bb8840e
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-01-12 11:43:59 +0100

    file-upload

--- client/client-drive/src/app/app.module.ts
+++ client/client-drive/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent
   ],
   imports: [
     BrowserModule
