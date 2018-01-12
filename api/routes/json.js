ac808c45ad918776db3b4df84a8344e0badff9ba
zahrsino <zaharialorenzo@gmail.com>
2018-01-12 12:44:47 +0100

    all type request work

--- api/routes/json.js
+++ api/routes/json.js
@@ -4,7 +4,6 @@
  * Set the AJAX calls to our server
  */
 
-
 /**
  * constant that takes the express modules
  * @type {[modules]}
@@ -35,98 +34,142 @@ const mongoose = require('mongoose');
 
 const Json = require('../models/json');
 
-/**** GET  ****/
- //Handle get calls
-/**
- * using router to register different well routes
- */
-//insert '/' because /json is insert in app
-//second method is a handler
-router.get('/', (req, res, next) => {
-    Json.find()
-        .exec()
-        .then(docs => {
-            console.log(docs);
-            res.status(200).json(doc);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        })
-});
-
-/**
- * get with a specific id
- */
- router.get('/:jsonId', (req, res, next) => {
-    const id = req.params.jsonId;
-    Json.findById(id)
-    .exec()
-    .then(doc => {
-<<<<<<< HEAD
-        console.log(doc);
-        res.status(200).json(doc);
-=======
-        if (doc) {
-            res.status(200).json(doc);
-        } else {
-            res.status(404).json({error: "No valid ID found"});
-        }
-        console.log(doc);
->>>>>>> 951c6b25d2ea3008f4323a785881b83d7857b441
+router
+    .route('/')
+    .get((req, res, next) => {
+        Json.find()
+            .select('_id nome cognome')
+            .exec()
+            .then(docs => {
+                const response = {
+                    count: docs.length,
+                    json: docs.map(doc => {
+                        return {
+                            _id: doc._id,
+                            nome: doc.nome,
+                            cognome: doc.cognome,
+                            request: {
+                                type: 'GET',
+                                url: 'http://localhost:3000/json/'+doc._id
+                            }
+                        }
+                    })
+                };
+                console.log(docs);
+                res.status(200).json(response);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                });
+            })
     })
-    .catch(error => {
-        console.log(error);
-        res.status(500).json({errore: error})
+    .post((req, res, next) => {
+        var id = generateID({prefix:"id-"});
+        const jsonData = new Json({
+            _id: new mongoose.Types.ObjectId(),
+            nome: req.body.nome,
+            cognome: req.body.cognome
+        });
+        jsonData
+            .save()
+            .then(result => {
+                console.log(result);
+                res.status(201).json({
+                    message: 'Json succesfully saved!',
+                    createdJson: {
+                        _id: result._id,
+                        nome: result.nome,
+                        cognome: result.cognome,
+                        request: {
+                            type: 'POST',
+                            url: 'http://localhost:3000/json/'+result._id
+                        }
+                    }
+                });
+            })
+            .catch(error => console.log(error));
     });
- });
 
- /**** POST ****/
- //Handle post calls
- router.post('/', (req, res, next) => {
-    var id = generateID({prefix:"id-"});
 
-    const jsonData = new Json({
-        _id: new mongoose.Types.ObjectId(),
-        nome: req.body.nome,
-        cognome: req.body.cognome
-    });
-
-    jsonData
-        .save()
-        .then(result => {
-            console.log("ciaooo");
-            console.log(result);
+router
+    .route('/:jsonID')
+    .get((req, res, next) => {
+        const id = req.params.jsonID;
+        Json.findById(id)
+        .select('_id nome cognome')
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            if (doc) {
+                res.status(200).json({
+                    json: doc,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/json'
+                    }
+                });
+            } else {
+                res.status(404).json({error: "No valid ID found"});
+            }
+            console.log(doc);
         })
-        .catch(error => console.log(error));
-
-        res.status(201).json({
-            message: 'handling POST request to /json',
-            createdJson: jsonData,
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({errore: error})
         });
-});
-
-/**** PUT ****/
- //Handle put calls
-/**
- * update a specific Id
- */
-router.put('/:jsonId', (req, res, next) => {
-    const id = req.params.jsonId;
-    if(fs.existsSync('./data/' + id + '.json')){
-        fs.writeFile('./data/' + id + '.json', JSON.stringify(req.body), (err) => {
-            errorHandling.checkErrorForPut(res, req, err);
-        });
-    } else {
-        res.status(404).json({});
-    }
-});
-router.delete('/:jsonId', (req, res, next) => {
-    res.status(200).json({
-        message: 'deleted!'
+     })
+     .patch((req, res, next) => {
+        const id = req.params.jsonID;
+        const updateOps = {}
+        for (const ops of req.body) {
+            updateOps[ops.propName] = ops.value;
+        }
+        Json.update({ _id: id}, {$set: updateOps})
+            .exec()
+            .then(result => {
+                console.log(result);
+                res.status(200).json({
+                    message: 'Json succesfully updated!',
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/json/'+result._id
+                    }
+                });
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                    error: err
+                })
+            })
+     })
+     .put((req, res, next) => {
+        const id = req.params.jsonID;
+        if(fs.existsSync('./data/' + id + '.json')){
+            fs.writeFile('./data/' + id + '.json', JSON.stringify(req.body), (err) => {
+                errorHandling.checkErrorForPut(res, req, err);
+            });
+        } else {
+            res.status(404).json({});
+        }
+    })
+    .delete((req, res, next) => {
+        const id = req.params.jsonID;
+        Json.remove({ _id: id })
+            .exec()
+            .then(result => {
+                console.log(result);
+                res.status(200).json({
+                    message: 'Json succesfully deleted!'
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ error: err })
+            })
     });
-});
+
 // export to module the routers
 module.exports = router;
