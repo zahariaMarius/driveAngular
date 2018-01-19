59e9b87c54cc1be479e6305db8dbd968cac9e57b
Pietro Chiarva <pietro.chiarva@edu.itspiemonte.it>
2018-01-19 11:20:30 +0100

    profile commit

--- client/client-drive/src/app/user-profile/user-profile.component.ts
+++ client/client-drive/src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ServerRequestService } from '../services/server-request/server-request.service';
 import { CheckCookieService } from '../services/check-cookie/check-cookie.service';
 import { log } from 'util';
+import { AfterViewInit, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,16 +11,39 @@ import { log } from 'util';
 })
 export class UserProfileComponent implements OnInit {
 
-  user: any;
+   user: any;
+   showSelected : boolean;
+   showEdit: boolean;
 
   constructor(
     private serverRequestService: ServerRequestService,
-    private cookie: CheckCookieService
-  ) { }
+    private cookie: CheckCookieService,
+    private elementRef:ElementRef
+  ) {
+      this.showSelected = false;
+      this.showEdit = true;
+ }
+
 
   ngOnInit() {
     this.getUserInformation();
   }
+
+  ngAfterViewInit() {
+  this.elementRef.nativeElement.querySelector('#edit')
+                                .addEventListener('click', this.onClick.bind(this));
+
+}
+
+onClick(event) {
+   this.showSelected = true;
+   this.showEdit = false;
+}
+
+saveData(name: string, surname: string, email: string, password: string){
+
+
+}
 
   getUserInformation() {
     this.serverRequestService.getUser().subscribe(
